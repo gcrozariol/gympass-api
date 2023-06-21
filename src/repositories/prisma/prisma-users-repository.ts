170f403a32f0437d528613a515ba4c3654b33aa0
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-06-21 05:45:41 -0300

    Minor refactoring - prisma users repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
-import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -24,7 +24,10 @@ export class PrismaUsersRepository implements UsersRepository {
   }
 
   async create(data: Prisma.UserCreateInput) {
-    const user = await prisma.user.create({ data })
+    const user = await prisma.user.create({
+      data,
+    })
+
     return user
   }
 }
