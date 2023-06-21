b4d6c2f2b0e8dba5030c98f491ae7c5a4844a2a4
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-06-21 13:27:45 -0300

    Create check-ins e2e test

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,38 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, it } from 'vitest'
+
+describe('Create check-in e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to check in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        description: 'Crazy gym.',
+        phone: '+1 647-675-3313',
+        latitude: 22.8922548,
+        longitude: 47.1502935,
+      },
+    })
+
+    await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: 22.8922548,
+        longitude: 47.1502935,
+      })
+      .expect(201)
+  })
+})
