51b56c5ab214390ec25a4529644ecfa804f93858
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-06-21 13:28:27 -0300

    Metrics check-ins e2e test

---
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,55 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Metrics check-in e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get the total number of check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        description: 'Crazy gym.',
+        phone: '+1 647-675-3313',
+        latitude: 22.8922548,
+        longitude: 47.1502935,
+      },
+    })
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gymId: gym.id,
+          userId: user.id,
+        },
+        {
+          gymId: gym.id,
+          userId: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: 22.8922548,
+        longitude: 47.1502935,
+      })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkInsCount).toEqual(2)
+  })
+})
